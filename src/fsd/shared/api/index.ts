39db6774b9d7601f13cf175ad493fc87a5ef9c7f
Divantikus <149273212+Divantikus@users.noreply.github.com>
2024-09-10 11:55:39 +0300

    changing the header design / starting to create a tag receipt request

--- src/fsd/shared/api/index.ts
+++ src/fsd/shared/api/index.ts
@@ -2,3 +2,4 @@ export { type RegisterData, type LoginData } from "./auth-service/authTypes";
 export { userActionsService } from "./user-action-service/user-action-service";
 export { authService } from "./auth-service/auth.service";
 export { artsService } from "./arts-service/arts.service";
+export { type Tag } from "./arts-service/artsTypes";
--- src/fsd/widgets/header/ui/Header.tsx
+++ src/fsd/widgets/header/ui/Header.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { usePathname } from "next/navigation";
-import notificationIcon from "@assets/header/notification.svg";
 import settingsIcon from "@assets/header/settings.svg";
 import profileIcon from "@assets/header/profile.svg";
 import createIcon from "@assets/header/create.svg";
@@ -18,27 +17,26 @@ export const Header = () => {
       <Link href={"/"} className={styles.link}>
         <div className={styles.logo}>A</div>
       </Link>
-      {!isCreatePage && (
-        <Link href={"/create"} className={styles.createPost}>
-          <Image src={createIcon} alt={"Иконка создания приложения"} />
-          <span className={styles.text}>Создать</span>
-        </Link>
-      )}
-      <div className={styles.otherButtons}>
-        <button>
-          <Image src={notificationIcon} alt={"Иконка колокольчика"} />
-        </button>
-        {isSettingsBtn && (
-          <button>
-            <Image src={settingsIcon} alt={"Иконка настроек"} />
-          </button>
-        )}
-        {!isSettingsBtn && (
-          <Link href={"/profile"}>
-            <Image src={profileIcon} alt={"Иконка профиля"} />
+      <div className={styles.linkWrap}>
+        {!isCreatePage && (
+          <Link href={"/create"} className={styles.createPost}>
+            <span className={styles.imgContainer}>
+              <Image src={createIcon} alt={"Иконка создания приложения"} />
+            </span>
           </Link>
         )}
       </div>
+
+      {isSettingsBtn && (
+        <button className={styles.profileOrSettingsBtn}>
+          <Image src={settingsIcon} alt={"Иконка настроек"} />
+        </button>
+      )}
+      {!isSettingsBtn && (
+        <Link href={"/profile"} className={styles.profileOrSettingsBtn}>
+          <Image src={profileIcon} alt={"Иконка профиля"} />
+        </Link>
+      )}
     </nav>
   );
 };
