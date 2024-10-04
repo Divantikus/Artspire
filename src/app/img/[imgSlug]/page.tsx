7ecc3b6c32279cea032a6baa179711241ec75997
Divantikus <149273212+Divantikus@users.noreply.github.com>
2024-10-04 12:55:52 +0300

    adding a plugin typing styles and getting started with the logic of the image page

---
+++ src/app/img/[imgSlug]/page.tsx
@@ -0,0 +1,5 @@
+import { PicturePage } from "@pages/img-page/index";
+
+export default function Page() {
+  return <PicturePage />;
+}
--- src/app/img/page.tsx
+++
@@ -1,5 +0,0 @@
-import { PicturePage } from "@/fsd/pages/img-page";
-
-export default function PageImg() {
-  return <PicturePage />;
-}
