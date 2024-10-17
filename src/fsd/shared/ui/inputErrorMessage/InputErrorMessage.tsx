22f9e7ab57ae1a4f26034c0e44397833538cdc8c
Divantikus <149273212+Divantikus@users.noreply.github.com>
2024-10-17 12:09:56 +0300

    changing the structure to switch/case

--- src/fsd/shared/ui/inputErrorMessage/InputErrorMessage.tsx
+++ src/fsd/shared/ui/inputErrorMessage/InputErrorMessage.tsx
@@ -1,29 +1,29 @@
 import { FieldError } from "react-hook-form";
 import styles from "./InputErrorMessage.module.scss";
+
 export const InputErrorMessage = ({ errorObj }: { errorObj: FieldError }) => {
   const { message, type } = errorObj;
 
-  if (type === "required")
-    return <p className={styles.errorMes}>Это поле является обязательным</p>;
-
-  if (type === "maxLength")
-    return (
-      <p className={styles.errorMes}>
-        Это поле не должно содержать больше 79 символов
-      </p>
-    );
-
-  if (type === "minLength")
-    return (
-      <p className={styles.errorMes}>
-        Это поле должно содержать минимум 8 символов
-      </p>
-    );
-
-  if (type === "pattern")
-    return (
-      <p className={styles.errorMes}>Некорректный адрес электронной почты</p>
-    );
-
-  return <p className={styles.errorMes}>{message}</p>;
+  switch (type) {
+    case "required":
+      return <p className={styles.errorMes}>Это поле является обязательным</p>;
+    case "maxLength":
+      return (
+        <p className={styles.errorMes}>
+          Это поле не должно содержать больше 79 символов
+        </p>
+      );
+    case "minLength":
+      return (
+        <p className={styles.errorMes}>
+          Это поле должно содержать минимум 8 символов
+        </p>
+      );
+    case "pattern":
+      return (
+        <p className={styles.errorMes}>Некорректный адрес электронной почты</p>
+      );
+    default:
+      return <p className={styles.errorMes}>{message}</p>;
+  }
 };
